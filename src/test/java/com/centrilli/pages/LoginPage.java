6ea5c0acc42c21892adabd38552abd8b39ecae4a
HuntermanTeam <jonsnow9091.fs@gmail.com>
2021-02-19 19:31:21 +0200

    added Login Page

--- src/test/java/com/centrilli/pages/LoginPage.java
+++ src/test/java/com/centrilli/pages/LoginPage.java
@@ -1,4 +1,33 @@
 package com.centrilli.pages;
 
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(id="login")
+    public WebElement userName;
+
+
+
+    @FindBy(id="password")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[@id=\"wrapwrap\"]/main/div/form/div[3]/button")
+    public WebElement submit;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+
+    }
 }
