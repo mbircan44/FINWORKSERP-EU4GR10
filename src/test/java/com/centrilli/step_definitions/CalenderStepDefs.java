ad3752ac54fb0628ef72522b596a4c26897934f7
HuntermanTeam <jonsnow9091.fs@gmail.com>
2021-02-19 14:46:58 +0200

    added Calender Events

---
+++ src/test/java/com/centrilli/step_definitions/CalenderStepDefs.java
@@ -0,0 +1,20 @@
+package com.centrilli.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class CalenderStepDefs {
+
+    @Given("the user is on the calender page")
+    public void the_user_is_on_the_calender_page() {
+
+
+    }
+
+    @When("the user should be able to create any info")
+    public void the_user_should_be_able_to_create_any_info() {
+
+
+    }
+
+}
---
+++ src/test/resources/features/calender.feature
@@ -0,0 +1,5 @@
+Feature: Calender Events
+  @wip
+  Scenario: users should be able to enter
+    Given the user is on the calender page
+    When the user should be able to create any info
