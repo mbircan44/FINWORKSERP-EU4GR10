7f8e389176ec5f42246665d2c21bf919b5f683b0
MEHMET OLGUN <sweetly35@gmail.com>
2021-02-26 21:06:38 +0300

    Adjusted login.feature

--- src/test/java/com/centrilli/step_definitions/LoginStepDefs.java
+++ src/test/java/com/centrilli/step_definitions/LoginStepDefs.java
@@ -27,21 +27,75 @@ public class LoginStepDefs {
         loginPage.login(username,password);
     }
 
-    @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() throws InterruptedException{
+    @Then("the user should be able to login as sales manager")
+    public void the_user_should_be_able_to_login_as_sales_manager() {
         BrowserUtils.waitFor(3);
         String actualUsername = Driver.get().findElement(By.className("oe_topbar_name")).getText();
-        Assert.assertEquals("verify username", "POSManager10", actualUsername);
-
+        Assert.assertEquals("verify username", "SalesManager15", actualUsername);
     }
+
     @When("the user enters the pos manager information")
-    public void the_user_enters_the_pos_manager_information() throws InterruptedException {
+    public void the_user_enters_the_pos_manager_information() {
         String username = ConfigurationReader.get("pos_manager_username");
         String password = ConfigurationReader.get("pos_manager_password");
 
         LoginPage loginPage = new LoginPage();
         loginPage.login(username,password);
+    }
 
+    @Then("the user should be able to login as pos manager")
+    public void the_user_should_be_able_to_login_as_pos_manager() {
+        BrowserUtils.waitFor(3);
+        String actualUsername = Driver.get().findElement(By.className("oe_topbar_name")).getText();
+        Assert.assertEquals("verify username", "POSManager10", actualUsername);
+    }
+
+    @When("the user enters the inventory manager information")
+    public void the_user_enters_the_inventory_manager_information() {
+        String username = ConfigurationReader.get("inventory_manager_username");
+        String password = ConfigurationReader.get("inventory_manager_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
+
+    @Then("the user should be able to login as inventory manager")
+    public void the_user_should_be_able_to_login_as_inventory_manager() {
+        BrowserUtils.waitFor(3);
+        String actualUsername = Driver.get().findElement(By.className("oe_topbar_name")).getText();
+        Assert.assertEquals("verify username", "InventoryManager10", actualUsername);
+    }
+
+    @When("the user enters the expenses manager information")
+    public void the_user_enters_the_expenses_manager_information() {
+        String username = ConfigurationReader.get("expenses_manager_username");
+        String password = ConfigurationReader.get("expenses_manager_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
+
+    @Then("the user should be able to login as expenses manager")
+    public void the_user_should_be_able_to_login_as_expenses_manager() {
+        BrowserUtils.waitFor(3);
+        String actualUsername = Driver.get().findElement(By.className("oe_topbar_name")).getText();
+        Assert.assertEquals("verify username", "ExpensesManager10", actualUsername);
+    }
+
+    @When("the user enters the manufacturing User information")
+    public void the_user_enters_the_manufacturing_User_information() {
+        String username = ConfigurationReader.get("manufacturing_User_username");
+        String password = ConfigurationReader.get("manufacturing_User_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
+
+    @Then("the user should be able to login as manufacturing User")
+    public void the_user_should_be_able_to_login_as_manufacturing_User() {
+        BrowserUtils.waitFor(3);
+        String actualUsername = Driver.get().findElement(By.className("oe_topbar_name")).getText();
+        Assert.assertEquals("verify username", "ManufacturingUser10", actualUsername);
     }
 
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -5,9 +5,21 @@ Feature:Users should be able to login
 @wip
     Scenario: Login as a sales manager
       When the user enters the sales manager information
-      Then the user should be able to login
+      Then the user should be able to login as sales manager
 
 @wip
   Scenario: Login as a pos manager
      When the user enters the pos manager information
-     Then the user should be able to login
+     Then the user should be able to login as pos manager
+
+  Scenario: Login as a inventory manager
+    When the user enters the inventory manager information
+    Then the user should be able to login as inventory manager
+
+  Scenario: login as a expenses manager
+    When the user enters the expenses manager information
+    Then the user should be able to login as expenses manager
+
+  Scenario: login as a manufacturing User
+    When the user enters the manufacturing User information
+    Then the user should be able to login as manufacturing User
